9fa51c501c83595d3b8bea4a8d445ea3fb1cc3f4
wonthechan <34850678+wonthechan@users.noreply.github.com>
2018-01-09 18:15:46 +0900

    공지사항 DB 구축

--- app/src/main/java/io/github/wonthechan/registeration/MainActivity.java
+++ app/src/main/java/io/github/wonthechan/registeration/MainActivity.java
@@ -1,5 +1,6 @@
 package io.github.wonthechan.registeration;
 
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentManager;
 import android.net.sip.SipSession;
 import android.support.v4.app.FragmentTransaction;
@@ -10,6 +11,14 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,16 +35,6 @@ public class MainActivity extends AppCompatActivity {
 
         noticeListView = (ListView) findViewById(R.id.noticeListView);
         noticeList = new ArrayList<Notice>();
-        noticeList.add(new Notice("공지사항입니다.", "임예찬", "2018-01-09")); // 예시 DATA
-        noticeList.add(new Notice("공지사항입니다.", "임예찬", "2018-01-09")); // 예시 DATA
-        noticeList.add(new Notice("공지사항입니다.", "임예찬", "2018-01-09")); // 예시 DATA
-        noticeList.add(new Notice("공지사항입니다.", "임예찬", "2018-01-09")); // 예시 DATA
-        noticeList.add(new Notice("공지사항입니다.", "임예찬", "2018-01-09")); // 예시 DATA
-        noticeList.add(new Notice("공지사항입니다.", "임예찬", "2018-01-09")); // 예시 DATA
-        noticeList.add(new Notice("공지사항입니다.", "임예찬", "2018-01-09")); // 예시 DATA
-        noticeList.add(new Notice("공지사항입니다.", "임예찬", "2018-01-09")); // 예시 DATA
-        noticeList.add(new Notice("공지사항입니다.", "임예찬", "2018-01-09")); // 예시 DATA
-        noticeList.add(new Notice("공지사항입니다.", "임예찬", "2018-01-09")); // 예시 DATA
         adapter = new NoticeListAdapter(getApplicationContext(), noticeList); // 어댑터에 위에서 작성한 noticeList가 차레대로 들어가게 된다. (매칭)
         noticeListView.setAdapter(adapter); // 어댑터에 들어가있는 모든 내용들이 각각의 view 형태로 리스트뷰에 보여진다.
 
@@ -85,5 +84,65 @@ public class MainActivity extends AppCompatActivity {
                 fragmentTransaction.commit();
             }
         });
+
+        new BackgroundTask().execute();
+    }
+    // 서브클래스 작성
+    class BackgroundTask extends AsyncTask<Void, Void, String>
+    {
+        String target;
+
+        @Override
+        protected void onPreExecute(){
+            target = "http://211.252.53.222/NoticeList.php";
+        }
+
+        @Override
+        protected String doInBackground(Void... voids){
+            try {
+                URL url = new URL(target);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection(); // 파싱
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String temp;
+                StringBuilder stringBuilder = new StringBuilder();
+                while((temp = bufferedReader.readLine()) != null)
+                {
+                    stringBuilder.append(temp + "\n");
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return stringBuilder.toString().trim();
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        public void onProgressUpdate(Void... values){
+            super.onProgressUpdate();
+        }
+        @Override
+        public void onPostExecute(String result){
+            try{
+                JSONObject jsonObject = new JSONObject(result);
+                JSONArray jsonArray = jsonObject.getJSONArray("response");
+                int count = 0;
+                String noticeContent, noticeName, noticeDate;
+                while(count < jsonArray.length())
+                {
+                    JSONObject object = jsonArray.getJSONObject(count);
+                    noticeContent = object.getString("noticeContent");
+                    noticeName = object.getString("noticeName");
+                    noticeDate= object.getString("noticeDate");
+                    Notice notice = new Notice(noticeContent, noticeName, noticeDate);
+                    noticeList.add(notice);
+                    count++;
+                }
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
